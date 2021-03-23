e566d6067cff83a0c60dedce7bb6160c03c15ff9
nevesalisson1 <nevesalissonn@gmail.com>
2021-03-23 17:19:30 -0300

    Implementação Entity Framework.

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using api_development.Models;
+
+namespace api_development.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
