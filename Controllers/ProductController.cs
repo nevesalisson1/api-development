20e4324d16993af2028eb9bf53d2f644ee42591e
nevesalisson1 <nevesalissonn@gmail.com>
2021-03-23 17:20:40 -0300

    Implementação GET e POST produto.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using api_development.Data;
+using api_development.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace api_development.Controllers
+{
+    [Route("api/produtos")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
+        {
+            var products = await context.Products.ToListAsync();
+            return products;
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Product>> Post([FromServices] DataContext context, [FromBody]Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Products.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace api_development.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O produto deve ter um nome.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O produto deve ter um valor válido.")]
+        [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
+        public float Valor_unitario { get; set; }
+
+        [Required(ErrorMessage = "Deve ser informada a quantidade em estoque do produto.")]
+        [Range(0, int.MaxValue, ErrorMessage = "A quantidade deve ser maior ou igual que zero" )]
+        public int Qtde_estoque { get; set; }
+
+        public float Valor_ultima_venda { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Data_ultima_venda { get; set; }
+
+    }
+}
