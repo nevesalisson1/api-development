fee86c4581229a2f721295437b789de79609285d
nevesalisson1 <nevesalissonn@gmail.com>
2021-03-24 21:29:21 -0300

    Implementação subtração da quantidades de produtos caso haja uma compra.

--- Controllers/CompraController.cs
+++ Controllers/CompraController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using api_development.Data;
 using api_development.Models;
+using System.Linq;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,8 +18,20 @@ namespace api_development.Controllers
         [Route("compras")]
         public async Task<Compra> Post([FromServices] DataContext context, [FromBody] Compra model)
         {
-            context.Compra.Add(model);
-            await context.SaveChangesAsync();
+            if (model.Cartao.CreditCard_Evaluation() == 1) // Se o cartão é válido.
+            {
+
+                var product = context.Products.Where(x => x.Id == model.Produto_id).FirstOrDefault();
+                if (product != null) // Se o produto existir
+                {
+                    if (model.Qtde_comprada <= product.Qtde_estoque) // Se a quantidade de compras é menor que a quantidade em estoque 
+                    {
+                        context.Compra.Add(model);
+                        context.Products.SingleOrDefault(x => x.Id == model.Produto_id).Qtde_estoque -= model.Qtde_comprada; // Diminui do banco a quantidade de estoque subtraindo a quantidade comprada
+                        await context.SaveChangesAsync();
+                    }
+                }
+            }
             return model;  
         }
 
